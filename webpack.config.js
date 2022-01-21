9633a4de97acaa11f029da4a24ba936724299413
rogue-dog <keshav.kashik20@nludelhi.ac.in>
2022-01-21 10:53:41 +0530

    Corrected tsconfig.json and Created webpack.config

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,28 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
 module.exports = {
   mode: "development",
   entry: "./src/index.ts",
+  output: {
+    path: __dirname + "/bundle",
+    filename: "show-margin-bundle.js",
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.tsx?/,
+        use: ["ts-loader"],
+        exclude: /(node_modules | bower_components)/,
+      },
+      {
+        test: /\.css/,
+        use: ["css-loader"],
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".ts", ".tsx", ".js", ".css"],
+  },
+  plugins: [new HtmlWebpackPlugin()],
 };
